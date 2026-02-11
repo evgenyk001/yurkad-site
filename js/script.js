b1a7ccafaca21310990c7ad08049d157d1811eae
evgenyk001 <ev.kozmenko@mail.ru>
2026-02-11 14:55:02 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('mousemove', (e) => {
     const x = (window.innerWidth / 2 - e.clientX) / 40;
     const y = (window.innerHeight / 2 - e.clientY) / 40;
 
-    style.transform = `translate(${x}px, ${y}px)`;
+    femida.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 
